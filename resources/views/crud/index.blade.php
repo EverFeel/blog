8d2f2c7a2fc0770dc65db03d9cf49dcfa864f2f8
unknown <radhit.andromeda@gmail.com>
2022-11-24 08:50:34 +0800

    update kamis

---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+@section('content')
+    <div class="bg-gray-300 min-h-screen">
+        <div class="max-w-7xl mx-auto bg-white flex flex-col px-10 min-h-screen py-10 gap-10">
+            <div class="">
+                <h1 class="text-lg font-semibold">judul</h1>
+                <span class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, option Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, tempore.</span>
+            </div>
+
+            <div class="">
+                <h1 class="text-lg font-semibold">judul</h1>
+                <span class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, option Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, tempore.</span>
+            </div>
+
+            <div class="">
+                <h1 class="text-lg font-semibold">judul</h1>
+                <span class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, option Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, tempore.</span>
+            </div>
+
+            <div class="">
+                <h1 class="text-lg font-semibold">judul</h1>
+                <span class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, option Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, tempore.</span>
+            </div>
+
+            <div class="">
+                <h1 class="text-lg font-semibold">judul</h1>
+                <span class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, option Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, tempore.</span>
+            </div>
+
+            <div class="">
+                <h1 class="text-lg font-semibold">judul</h1>
+                <span class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, option Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, tempore.</span>
+            </div>
+        </div>
+    </div>
+@endsection
